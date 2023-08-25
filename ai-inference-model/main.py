819e90a7b6349aa2fa9ecf44d0a8316b01f1a5a9
Subin Erattakulangara <s.erattakulangara@kitware.com>
2023-08-25 14:55:54 -0400

    api supports numpy array inputs

--- ai-inference-model/main.py
+++ ai-inference-model/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import numpy as np
+from typing import List
 
 app = FastAPI()
 
@@ -7,11 +9,43 @@ app = FastAPI()
 def read_root():
     return {"This code allows the AI model to be packed as an API"}
 
-class InputData(BaseModel):
-    data: dict
+class NumpyArray(BaseModel):
+    """
+    Pydantic model for receiving a NumPy array as JSON input.
 
-@app.post("/aimodel/")
-async def process_data(input_data: InputData):
-    # You can process the input data here
-    processed_data = input_data.data
-    return processed_data
+    Attributes:
+        numpy_array (List[List[float]]): A 2D list representing the NumPy array.
+    """
+    numpy_array: List[List[float]]
+
+@app.post("/process_tiles/")
+async def process_data(input_data: NumpyArray):
+    """
+    Process a NumPy array received as JSON input.
+
+    Args:
+        input_data (NumpyArray): JSON input containing a NumPy array.
+
+    Returns:
+        List[List[float]]: Processed NumPy array.
+
+    Raises:
+        HTTPException: If there is an issue with the input data format.
+    """
+    try:
+        # Access the JSON data as a list and convert it to a NumPy array
+        json_data = input_data.numpy_array
+        numpy_array = np.array(json_data, dtype=np.float64)
+        
+        # Example: Multiply each element of the NumPy array by 2
+        processed_numpy_array = numpy_array * 2
+
+        ############# Fit your tile processing model here ################
+        # You can add your tile processing logic here.
+
+        #################################################################
+        
+        return processed_numpy_array.tolist()
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid data")
